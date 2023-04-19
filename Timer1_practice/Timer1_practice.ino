b369b99f411044813d0d1fc2b8b77f2dc2e2c39a
run0122 <run0122@naver.com>
2023-04-19 11:08:09 +0900

    3

--- Timer1_practice/Timer1_practice.ino
+++ Timer1_practice/Timer1_practice.ino
@@ -6,10 +6,12 @@ void setup() {
   Timer1.initialize();
   Timer1.pwm(LED, 0);
 
-  Timer1.setPeriod(1000000/1000);
-  Timer1.setPwmDuty(LED, 900);
+  Timer1.setPeriod(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for (int t_high = 0; t_high <= 1023; t_high++) {
+    Timer1.setPwmDuty(LED, t_high);
+    delay(1);
+  }
 }
