57e17e3259dd97f0638b9a269a2fb7a37b5ea0fb
run0122 <run0122@naver.com>
2023-04-19 10:59:18 +0900

    1

--- Timer1_practice/Timer1_practice.ino
+++ Timer1_practice/Timer1_practice.ino
@@ -1,9 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <TimerOne.h>
 
+const int LED = 9;
+
+void setup() {
+  Timer1.initialize();
+  Timer1.pwm(LED, 0);
+
+  Timer1.setPeriod(1000000);
+  Timer1.setPwmDuty(LED, 511);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
